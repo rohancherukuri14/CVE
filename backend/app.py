79a238ac66fd15a59a3cf2ce9a216345cba326dc
Rohan Cherukuri <rohancherukuri@rohans-mbp.lan>
2023-07-26 10:29:31 -0400

    working call to gpt

--- backend/app.py
+++ backend/app.py
@@ -1,17 +1,54 @@
 from flask import Flask, request, jsonify
 import subprocess
+import openai
+import sys
+import utils
+import json
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins='http://localhost:3000', allow_headers=["Content-Type"])
 
-@app.route('/api/chatbot', methods=['POST'])
+def get_config_key():
+    with open('../config.json') as f:
+        config_data = json.load(f)
+    return config_data["api_key"]
+
+openai.api_key = get_config_key()
+
+def add_cors_headers(response):
+    # Allow requests from the specific origin (your React app's origin)
+    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:3000'
+    # Allow the Content-Type header for the preflight request
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
+@app.route('/api/chatbot', methods=['POST', 'OPTIONS'])
 def run_chatbot():
-    data = request.get_json()
-    user_message = data.get('message')
+    if request.method == 'OPTIONS':
+        response = jsonify({})
+    else:
+        data = request.get_json()
+        user_message = data.get('message')
 
-    # Run the Python script (example: my_script.py) with user_message as an argument
-    result = subprocess.run(['python3', '../my_script.py', user_message], capture_output=True, text=True)
+        # Run the Python script (example: my_script.py) with user_message as an argument
+        result = get_response(user_message)
 
-    return jsonify({'message': result.stdout.strip()})
+        response = jsonify({'message': result})
+
+    response = add_cors_headers(response)
+    return response
+
+
+def get_response(code):
+    message = [{"role": "system", "content" : "You are an expert cybersecurity analyst. The user will input code and you will give possible vulnerabilities in the code.\nKnowledge cutoff: 2021-09-01\nCurrent date: 2023-03-02"},
+                {"role": "user", "content" : code}]
+    completion = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = message,
+    )
+    return completion.choices[0].message["content"]
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- chatbot-app/src/components/Chatbot.js
+++ chatbot-app/src/components/Chatbot.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './Chatbot.css';
 
 const Chatbot = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const sendMessage = async () => {
+    if (inputValue.trim() === '') return;
+
+    // Add the user message to the state
+    const newMessage = { text: inputValue, fromUser: true };
+    setMessages([...messages, newMessage]);
+    setInputValue('');
+
+    // Make the API call to the Flask server
+    try {
+      const response = await axios.post('http://127.0.0.1:5000/api/chatbot', { message: inputValue });
+      const botMessage = { text: response.data.message, fromUser: false };
+      setMessages([...messages, botMessage]);
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  };
+
   return (
     <div className="chatbot-container">
       <div className="chatbot-messages">
-        <div className="user-message">Hello, how can I help you?</div>
-        <div className="bot-message">Hi! I'm a chatbot.</div>
+        {messages.map((message, index) => (
+          <div key={index} className={message.fromUser ? 'user-message' : 'bot-message'}>
+            {message.text}
+          </div>
+        ))}
       </div>
       <div className="chatbot-input">
-        <input type="text" placeholder="Paste code..." />
-        <button>Send</button>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === 'Enter') {
+              sendMessage();
+            }
+          }}
+          placeholder="Paste code..."
+        />
+        <button onClick={sendMessage}>Send</button>
       </div>
     </div>
   );
