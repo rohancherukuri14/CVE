e43597ffb09cf635b47e38b095b6747b8f7f1cbd
Rohan Cherukuri <rohancherukuri@rohans-mbp.lan>
2023-08-27 18:12:48 -0400

    new output.jsonl

--- backend/query_CVE.py
+++ backend/query_CVE.py
@@ -13,6 +13,7 @@ import re
 import csv
 import ast
 import json
+import fileinput
 
 def create_connection(db_file):
     """
@@ -31,26 +32,29 @@ def create_connection(db_file):
 conn = create_connection("CVEfixes.db")
 
 query = """
-SELECT cv.cve_id, f.filename, f.num_lines_added, f.num_lines_deleted, f.code_before, f.code_after, cc.cwe_id 
-FROM file_change f, commits c, fixes fx, cve cv, cwe_classification cc
+SELECT  f.code_before, f.code_after, cw.extended_description
+FROM file_change f, commits c, fixes fx, cve cv, cwe_classification cc, cwe cw
 WHERE f.hash = c.hash 
 AND c.hash = fx.hash 
 AND fx.cve_id = cv.cve_id 
 AND cv.cve_id = cc.cve_id
+AND cw.cwe_id = cc.cwe_id
 AND f.code_before != "None"
 AND f.code_after != "None"
 """
+query2 = """
+SELECT * FROM file_change f
+"""
 code_fixes = pd.read_sql_query(query, conn)
-print(len(code_fixes))
+print(code_fixes.columns)
 
 json_entries = []
 
-# Iterate through the first 1000 rows of the DataFrame
 count = 0
 for index, row in code_fixes.iterrows():
-    if len(row['code_before']) < 4000 and len(row['code_after']) < 1000:
+    if len(row['code_before']) < 4000 and len(row['code_after']) < 1000 and len(row['extended_description']) > 5 and "Insufficient Information" not in row['extended_description']:
         count += 1
-        entry = {"messages": [{"role": "system", "content": "Provide new code to fix the vulnerabilities in the code given."},{"role": "user", "content": row['code_before']},{"role": "assistant", "content": row['code_after']}]}
+        entry = {"messages": [{"role": "system", "content": "Provide new code to fix the vulnerabilities in the code given."},{"role": "user", "content": row['code_before']},{"role": "assistant", "content": row['extended_description'] + "Here is the fixed code: " + row['code_after']}]}
         json_entries.append(entry)
     if count > 1099:
         break
@@ -64,9 +68,40 @@ def prepare_data(dictionary_data, final_file_name):
             outfile.write('\n')       
 
 # Write the JSON entries to a file
-prepare_data(json_entries[:1000], "output.jsonl")
-prepare_data(json_entries[1000:], "validation.jsonl")
+prepare_data(json_entries[:1000], "output_temp.jsonl")
+prepare_data(json_entries[1000:], "validation_temp.jsonl")
 
+
+def process_content(content):
+    # Replace unicode escape sequences with their corresponding characters
+    content = content.replace("\\n", "").replace("\\t", "")
+    content = content.replace("\n", "").replace("\t", "")
+    return content
+
+# Read the JSONL file and fix the content
+with open("output_temp.jsonl", "r") as input_file, open("output.jsonl", "w") as output_file:
+    for line in input_file:
+        data = json.loads(line)
+        messages = data["messages"]
+        
+        for message in messages:
+            message["content"] = process_content(message["content"])
+        
+        # Write the updated JSON structure back to the output file
+        json.dump(data, output_file)
+        output_file.write("\n")
+
+with open("validation_temp.jsonl", "r") as input_file, open("validation.jsonl", "w") as output_file:
+    for line in input_file:
+        data = json.loads(line)
+        messages = data["messages"]
+        
+        for message in messages:
+            message["content"] = process_content(message["content"])
+        
+        # Write the updated JSON structure back to the output file
+        json.dump(data, output_file)
+        output_file.write("\n")
 
 
 
